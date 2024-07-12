3ec85424a31fe31122b2ca11384e777dcd359e35
Eric Espie <eric.espie@combodo.com>
2024-07-12 15:20:55 +0200

    Modules creation

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'combodo/combodo-mfa-totp',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '0aaef3d1b363c95610fc1ead426f4abb3e384e34',
+        'type' => 'itop-extension',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'combodo/combodo-mfa-totp' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '0aaef3d1b363c95610fc1ead426f4abb3e384e34',
+            'type' => 'itop-extension',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
