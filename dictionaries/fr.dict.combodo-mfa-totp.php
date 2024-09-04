80f5c50c24a9d3d2c8f8df1744334ddac383b93c
Eric Espie <eric.espie@combodo.com>
2024-09-04 14:07:29 +0200

    dictionaries

--- dictionaries/fr.dict.combodo-mfa-totp.php
+++ dictionaries/fr.dict.combodo-mfa-totp.php
@@ -9,6 +9,8 @@
 Dict::Add('FR FR', 'French', 'Français', array(
 	'MFA:login:switch:label:MFAUserSettingsTOTPApp' => 'Use authenticator app',
 	'MFA:login:switch:label:MFAUserSettingsTOTPMail' => 'Send verification code by email',
+	'combodo-mfa-totp/Operation:MFATOTPMailConfig/Title' => 'Configure MFA by email',
+	'combodo-mfa-totp/Operation:MFATOTPAppConfig/Title' => 'Configure authenticator app',
 
 	'MFATOTP:Redirection:Title' => 'Redirecting to home page...',
 	'MFATOTP:Validated' => 'Authenticator app Validated',
@@ -30,10 +32,22 @@ Dict::Add('FR FR', 'French', 'Français', array(
 	'MFATOTP:Mail:EnterCode' => 'Enter code received by email',
 	'MFATOTP:Mail:Config:Title' => 'Configure MFA by email',
 	'MFATOTP:Mail:Label' => 'Email',
-	'MFATOTP:Mail:CodeValidation:Title' => 'Enter received code sent by email to %1$s',
+	'MFATOTP:Mail:CodeValidation:Title' => 'Enter code received by email at %1$s',
 	'MFATOTP:Mail:ResendEmail:Link' => 'To receive another code at %1$s: click here',
+	'MFATOTP:Mail:ResendEmail:Button' => 'Send validation code',
+	'MFATOTP:Mail:ResendEmail:Done' => 'Email sent',
 	'MFATOTP:Mail:Validation:Title' => 'MFA by email',
+	'MFATOTP:Mail:Settings:Title' => 'Email settings',
+	'MFATOTP:Mail:Settings:email:label' => 'User email',
+	'MFATOTP:Mail:Settings:Saved:Done' => 'MFA email has been saved successfully with email %1$s',
+	'MFATOTP:Mail:Validation' => 'Validation',
+	'MFATOTP:Mail:CodeValidation:explain' => 'Enter code received by email',
 
+	'MFATOTP:Mail:EmailSubject' => '%1$s - Code to connect. Expires at %3$s',
+	'MFATOTP:Mail:EmailBody' => '<body><p>The code to connect to %1$s as %2$s is</p><strong>%4$s</strong><p></p><p>This code is valid until %3$s</p><p>If you are not trying to connect to %1$s, please contact your administrator.</p>',
+
+	'MFATOTP:Error:SendMailFailed' => 'Error: Sending MFA email for code has failed.',
+	'MFATOTP:Error:SaveSettingsFailed' => 'Error: Saving MFA email has failed. (probably a wrong email format)',
 
 	'Class:MFAUserSettingsTOTPApp' => 'MFA TOTP by application',
 	'Class:MFAUserSettingsTOTPMail' => 'MFA TOTP by mail',
